f2fb9a879679c751142477ea1ba9a96b672fb553
geyujie <961211746@qq.com>
2024-04-01 17:32:15 +0800

    perf: 移除无用代码

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const isProd = process.env.NODE_ENV === "production"
-const plugins = [
-    new MiniCssExtractPlugin({
-        filename: 'static/css/[name].[contenthash].css',
-        chunkFilename: 'static/css/[name].[contenthash].chunk.css',
-    }),
-]
-plugins.push(new HtmlWebpackPlugin({ title: "monitor-sdk", template: './template.html' }))
+// const plugins = [
+//     new MiniCssExtractPlugin({
+//         filename: 'static/css/[name].[contenthash].css',
+//         chunkFilename: 'static/css/[name].[contenthash].chunk.css',
+//     }),
+// ]
+// plugins.push(new HtmlWebpackPlugin({ title: "monitor-sdk", template: './template.html' }))
 module.exports = {
     mode: process.env.NODE_ENV,
     devtool: isProd ? 'hidden-source-map' : 'source-map',
@@ -41,27 +41,27 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/
             },
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
-            },
-            {
-                test: /\.less$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
-            },
-            {
-                test: /\.(gif|jpe?g|png|svg)$/i,
-                type: "asset/resource",
-                generator: {
-                    filename: "static/resource/[name].[hash][ext]",
-                },
-            },
+            // {
+            //     test: /\.css$/i,
+            //     use: [MiniCssExtractPlugin.loader, "css-loader"],
+            // },
+            // {
+            //     test: /\.less$/i,
+            //     use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
+            // },
+            // {
+            //     test: /\.(gif|jpe?g|png|svg)$/i,
+            //     type: "asset/resource",
+            //     generator: {
+            //         filename: "static/resource/[name].[hash][ext]",
+            //     },
+            // },
         ],
     },
     resolve: {
         extensions: ['.ts', '.js']
     },
-    plugins: plugins,
+    // plugins: plugins,
     devServer: {
         port: 8081,
         open: false,
